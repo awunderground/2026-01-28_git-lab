69e16db9ac1f5cd917fdc1a41b1d4af6b9fe7e1c
katebarnes273 <keb314@georgetown.edu>
2026-01-28 20:21:50 -0500

    Add files via upload

---
+++ ppls_users.R
@@ -0,0 +1,18 @@
+## 2026-01-28 Git Lab
+
+#Vector of GitHub usernames for the class // hardcoded it sorry lol
+class.git.users <- c(
+  "awunderground",
+  "katebarnes273",
+  "jwilling51",
+  "ethansager",
+  "swk2457",
+  "mv877",
+  "sh1930",
+  "yuyanhe0202",
+  "Yi-Dou-513",
+  "kng43"
+)
+
+#Printing usernames to console
+print(class.git.users)
